220836de8fc348ce6be6defc146f0072ec2d4da1
app-kaisha <app.kaihatsusha@gmail.com>
2025-12-30 12:30:45 +0000

    Ch1 Fundamentals Bindings complete

--- TwoWayBindings/ContentView.swift
+++ TwoWayBindings/ContentView.swift
@@ -11,18 +11,53 @@ import SwiftUI
 struct ContentView: View {
     
     @State private var pressCount = 0
+    @State private var toggleIsOn = true
+    @State private var name = ""
+    @State private var selectedColour: Color = .red
+    @State private var selectedDate = Date()
+    @State private var stepperValue = 1
     
     var body: some View {
         VStack {
+            Spacer()
+            
             Text("Press count: \(pressCount)")
                 
             Button("Press Me") {
                 pressCount += 1
             }
             .buttonStyle(.borderedProminent)
+            
+            Spacer()
+            
+            TextField("Enter a name", text: $name)
+                .textFieldStyle(.roundedBorder)
+            Text("Name entered: \(name)")
+            
+            Spacer()
+            
+            Toggle("Toggle is \(toggleIsOn ? "on" : "off"):", isOn: $toggleIsOn)
+            
+            Spacer()
+            Rectangle()
+                .fill(selectedColour)
+                .frame(width: 100, height: 100)
+            ColorPicker("Pick a Rectangle colour: ", selection: $selectedColour)
+            
+            Spacer()
+            
+            DatePicker("Date:", selection: $selectedDate)
+            Text("Selected date is: \(selectedDate.formatted(date: .abbreviated, time: .shortened))")
+                .minimumScaleFactor(0.5)
+            
+            Spacer()
+            
+            Stepper("Stepper value: \(stepperValue)", value: $stepperValue, in: 1...10)
+            
+            Spacer()
         }
         .font(.title2)
-       
+        .padding()
     }
 }
 
