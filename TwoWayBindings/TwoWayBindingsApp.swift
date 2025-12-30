36bf92da39743976af0e6b5fce58bcc639395c89
app-kaisha <app.kaihatsusha@gmail.com>
2025-12-30 12:03:25 +0000

    Initial Commit

---
+++ TwoWayBindings/TwoWayBindingsApp.swift
@@ -0,0 +1,18 @@
+//
+//  TwoWayBindingsApp.swift
+//  TwoWayBindings
+//
+//  Created by app-kaihatsusha on 30/12/2025.
+//  Copyright © 2025 app-kaihatsusha. All rights reserved.
+//
+
+import SwiftUI
+
+@main
+struct TwoWayBindingsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
